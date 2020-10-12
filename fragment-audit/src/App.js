3902435f0c2cb23f0e4a3ff36c22ff925b479956
jtx007 <jamesjacobthomas7@gmail.com>
2020-10-12 15:47:50 -0400

    removed misc files

---
+++ fragment-audit/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import FragmentComponent from './FragmentComponent'
+import EmptyFragment from './EmptyFragment'
+
+const App = () => {
+    return (
+        <>
+            <FragmentComponent />
+            <EmptyFragment />
+        </>
+    )
+}
+
+export default App
